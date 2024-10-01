3506b8ffea022bb9281a627a3642f83e19e9337a
AbhinabaSantra <abhinabasantra84@gmail.com>
2024-10-01 10:14:06 +0530

    removed some bugs in renderging prev expenses and the bugs in css mobile

--- scripts/ExpensePageScripts/prevExpense.js
+++ scripts/ExpensePageScripts/prevExpense.js
@@ -3,6 +3,10 @@ import { renderPrevExpenseHTML } from "./utils/renderPrevExpenseHTML.js";
 export function displayPrevExpense(userName) {
   const expenseList = JSON.parse(localStorage.getItem("expenseList")) || {};
   const userExpense = expenseList[userName];
+  if (!userExpense || userExpense.length === 0) {
+    document.querySelector(".prevExpense").style.display = "none";
+    return;
+  }
   const reversedUserExpense = userExpense.reverse();
   renderPrevExpenseHTML(reversedUserExpense, userName);
 }

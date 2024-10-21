5e71e16edfcc337645be922f250cd1380f53143a
AbhinabaSantra <abhinabasantra84@gmail.com>
2024-10-21 22:21:24 +0530

    minor debugging

--- scripts/SettingsPageScripts/preferenceSection.js
+++ scripts/SettingsPageScripts/preferenceSection.js
@@ -2,6 +2,8 @@ const monthlyRadio = document.querySelector(".monthlyPreferenceInput");
 const annualRadio = document.querySelector(".annualPreferenceInput");
 const saveButton = document.querySelector(".preferenceButton");
 export function preferenceSectionRender(userName) {
+  autoCheckedSavedPreference(userName);
+  checkBeforeReload();
   saveButton.addEventListener("click", () => {
     let checkedElem;
     [monthlyRadio, annualRadio].forEach((elem) => {
@@ -23,3 +25,26 @@ function preferenceUpdate(userName, checkElem) {
   userPreference.dataView = checkElem;
   localStorage.setItem("preferences", JSON.stringify(preferences));
 }
+
+function autoCheckedSavedPreference(userName) {
+  const preferences = JSON.parse(localStorage.getItem("preferences")) || {};
+  const userPreference = preferences[userName];
+  const preferenceView = userPreference.dataView || "monthly";
+  [monthlyRadio, annualRadio].forEach((elem) => {
+    if (preferenceView === elem.value) {
+      elem.checked = true;
+    }
+  });
+}
+
+// function checkBeforeReload() {
+//   let change = false;
+//   document.querySelector("main").addEventListener("change", () => {
+//     change = true;
+//   });
+//   window.addEventListener("beforeunload", (event) => {
+//     event.preventDefault();
+//     event.returnValue = "hello";
+//     alert("hello");
+//   });
+// }
